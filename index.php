4ab0c9de2f2cc8fa183d0fb77cbfbb7abcccfa41
ComfyHe-nry <133361990+ComfyHe-nry@users.noreply.github.com>
2023-05-30 20:15:38 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+  <meta http-equiv="CONTENT-TYPE" content="text/html; charset=UTF-8">
+  <link rel="stylesheet" href="styles/style.css"/>
+  
+</head>
+<body>
+  
+  <?php
+$name = "<h1>Henry, Mikpedigeabasi Victor</h1>";
+$regno = "2021/HND/34147/CS<br>";
+$soo = "AKWA IBOM STATE<br>";
+$project = "<strong>Design and Implementation of online tickets for stadium based management system (a case study of Pa-Ngele oruta township stadium, Abakiliki</strong>";
+echo $name;
+echo $regno;
+echo $soo;
+echo $project;
+?>
+</body>
+</html>
+
+
+Henry, Mikpedigeabasi Victor
+2021/HND/34174/CS
+AKWA IBOM
